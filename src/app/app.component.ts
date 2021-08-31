6fad43b7deb3466d9f1a3ec97099527a3098a488
ShevchenkoT <tarashevchuk@gmail.com>
2021-09-01 00:32:18 +0300

    created project and added form, inputs, and validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MyValidators } from './shared/my.validators';
 
+export interface mainData {
+  mainTime: number,
+  sundayTime: number,
+  nightTime: number,
+  weekendTime: number,
+  overTime: number,
+
+  premium: number,
+  bonus: number,
+  advance: number,
+  uahPerHour: number,
+  averageSalary: number,
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'salaryCalculator';
+export class AppComponent implements OnInit {
+  form!: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      position: new FormControl(null, [Validators.required]),
+      mainTime: new FormControl(null, [Validators.required, Validators.maxLength(3), MyValidators.ifInt]),
+      sundayTime: new FormControl(null, [Validators.required, Validators.maxLength(3), MyValidators.ifInt]),
+      nightTime: new FormControl(null, [Validators.required, Validators.maxLength(3), MyValidators.ifInt]),
+      weekendTime: new FormControl(null, [Validators.required, Validators.maxLength(3), MyValidators.ifInt]),
+      overTime: new FormControl(null, [Validators.required, Validators.maxLength(3), MyValidators.ifInt]),
+
+      premium: new FormControl(null, [Validators.required, Validators.maxLength(4), MyValidators.ifIntCommaPoint]),
+      bonus: new FormControl(null, [Validators.required, Validators.maxLength(4), MyValidators.ifIntCommaPoint]),
+      advance: new FormControl(null, [Validators.required, Validators.maxLength(5), MyValidators.ifInt]),
+    })
+  }
+  onSubmit() {
+    const newData: mainData = {
+      mainTime: +this.form.value.mainTime,
+      sundayTime: +this.form.value.sundayTime,
+      nightTime: +this.form.value.nightTime,
+      weekendTime: +this.form.value.weekendTime,
+      overTime: +this.form.value.overTime,
+
+      premium: parseFloat(this.form.value.premium.replace(/[^\d\.\-]/g, ".")),
+      bonus: parseFloat(this.form.value.bonus.replace(/[^\d\.\-]/g, ".")),
+      advance: this.form.value.advance,
+      uahPerHour: this.getUahPerHour(this.form.value.position),
+      averageSalary: this.getAverageSalary(this.form.value.position),
+    }
+
+    console.log("hello");
+    console.log(newData);
+  }
+
+  getUahPerHour(position: string): number {
+
+
+    return 54.2
+  }
+  getAverageSalary(position: string): number {
+    return 6500
+  }
 }
---
+++ src/app/shared/my.validators.ts
@@ -0,0 +1,19 @@
+import { FormControl } from '@angular/forms';
+
+export class MyValidators {
+  static ifInt(control: FormControl): { [key: string]: boolean } {
+    if (control.value) {
+      return /\D/.test(control.value) ? { isNotInteger: true } : null as any;
+    }
+    return null as any;
+  }
+
+  static ifIntCommaPoint(control: FormControl): { [key: string]: boolean } {
+    if (control.value) {
+      return !/^[0-9]{1,2}([,.][0-9]{1,2})?$/.test(control.value) ? { isNotInteger: true } : null as any;
+    }
+    return null as any;
+  }
+}
+
+
